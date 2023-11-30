ba047324a1966fe0f50e80ad8f3d767a4f3cf64d
Saad Saiyed <saadsaiyed7@gmail.com>
2023-11-30 17:19:30 -0500

    renamed some files and add db_connection.py

--- test.py
+++ test.py
@@ -2,14 +2,15 @@
 import replicate, os, logging, tiktoken
 from transformers import AutoTokenizer
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.basicConfig(filename='./.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+import load_db
 
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.basicConfig(filename='./.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 model = "01-ai/Yi-34B-chat"
 sub_model = "914692bbe8a8e2b91a4e44203e70d170c9c5ccc1359b283c84b0ec8d47819a46"
 
-
+# this was just for testing
 def import_db():
     def read_sql_file(file_path):
         with open(file_path, 'r') as sql_file:
@@ -23,45 +24,39 @@ def import_db():
         print(f"\n\nToken count: {token_count}\n")
         return token_count
 
-    sql_query = read_sql_file("./db.sql")
+    sql_query = read_sql_file("./sample.sql")
 
-    chunk_size = 0
-    chunk = ""
-    for i in sql_query.split("\n"):
-        chunk += i + "\n"
-        chunk_size += 1
-
-        if chunk_size >= 100:
-            print(chunk)
-            token_counter(chunk)
-            chunk = ""
-            chunk_size = 0
-            input()
-    # logging.debug(f"sql_query {type(sql_query)}: {sql_query}")
+    token_counter(sql_query)
     return sql_query
 
     
 def main():
-    data = import_db()
+    prompt = input("What do you want to know about your Database?\n")
+    
+    data = load_db.get_relevant_data(prompt)
+    
     prompt_template = '''
     <|im_start|>system
     You are a Data Scientist that can understand DataBase in-depth. You will be given exported data from MySQL including the relation between the tables.
     Your job is to answer the question precisely and solely based on the data given.
     Following is the mysql exported data.
-    ```mysql
-    '''+data+'''```<|im_end|>
+    ```
+    '''+data+'''
+    ```
+    <|im_end|>
     <|im_start|>user
-    {prompt}<|im_end|>
+    {prompt}
+    <|im_end|>
     <|im_start|>assistant
     '''
 
-    while False:
+    while True:
         output = replicate.run(
             model.lower()+":"+sub_model.lower(),
             input={
                 "top_k": 50,
                 "top_p": 0.8,
-                "prompt": input("What do you want to know about your Database?\n"),
+                "prompt": prompt,
                 "temperature": 0.3,
                 "max_new_tokens": 1024,
                 "prompt_template": prompt_template,
@@ -71,10 +66,8 @@ def main():
 
         # Convert the generator to a list
         output_list = list(output)
+        response = ""
+        for o in output_list: response += o
 
-        # Print the results
-        for o in output_list:
-            print(o, end="")
-        print()
-
+        print(response, end="\n\n")
 main()
